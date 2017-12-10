8e1fd5838665478dfa5e4fc2fdabdd442977f3a2
Vladimir Konchakovsky <wowkaster@gmail.com>
2017-12-10 17:40:19 +0200

    Убрал код, который занимался регистрацией permishens из таблицы БД

--- publish/Providers/AuthServiceProvider.php
+++ publish/Providers/AuthServiceProvider.php
@@ -17,38 +17,4 @@ class AuthServiceProvider extends ServiceProvider
         'App\Model' => 'App\Policies\ModelPolicy',
     ];
 
-    /**
-     * Register any application authentication / authorization services.
-     *
-     * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
-     *
-     * @return void
-     */
-    public function boot(GateContract $gate)
-    {
-        parent::registerPolicies($gate);
-
-        try {
-            if (\Schema::hasTable('permissions')) {
-                // Dynamically register permissions with Laravel's Gate.
-                foreach ($this->getPermissions() as $permission) {
-                    $gate->define($permission->name, function ($user) use ($permission) {
-                        return $user->hasPermission($permission);
-                    });
-                }
-            }
-        } catch (\Illuminate\Database\QueryException $ex) {
-            return;
-        }
-    }
-
-    /**
-     * Fetch the collection of site permissions.
-     *
-     * @return \Illuminate\Database\Eloquent\Collection
-     */
-    protected function getPermissions()
-    {
-        return Permission::with('roles')->get();
-    }
 }
