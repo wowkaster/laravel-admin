17cce27ac6a222d54421fefa34996cd6315345b0
Vladimir Konchakovsky <wowkaster@gmail.com>
2017-12-10 17:42:01 +0200

    Убрал генерацию route

--- src/LaravelAdminCommand.php
+++ src/LaravelAdminCommand.php
@@ -62,9 +62,6 @@ class LaravelAdminCommand extends Command
         $routes =
             <<<EOD
 Route::get('admin', 'Admin\\AdminController@index');
-Route::resource('admin/roles', 'Admin\\RolesController');
-Route::resource('admin/permissions', 'Admin\\PermissionsController');
-Route::resource('admin/users', 'Admin\\UsersController');
 Route::get('admin/generator', ['uses' => '\Appzcoder\LaravelAdmin\Controllers\ProcessController@getGenerator']);
 Route::post('admin/generator', ['uses' => '\Appzcoder\LaravelAdmin\Controllers\ProcessController@postGenerator']);
 EOD;
